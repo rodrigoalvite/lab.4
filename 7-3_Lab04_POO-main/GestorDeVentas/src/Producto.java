2b53458c70d1ac6fcbf9f48bced64cde05744758
rodrigoalvite <rodrigo.alvite07@gmail.com>
2026-04-30 17:08:38 +0000

    Update Producto.java

--- 7-3_Lab04_POO-main/GestorDeVentas/src/Producto.java
+++ 7-3_Lab04_POO-main/GestorDeVentas/src/Producto.java
@@ -1,15 +1,15 @@
 public class Producto {
-    private static int CantProducto ;
-    private static int contador = 99;
-    private int idProducto;
-    private String nombre;
-    private double precio;
+    private static int CantProducto ;//cuenta los productos que se crearon
+    private static int contador = 99;//sirve para generar ids automáticos
+    private int idProducto;//id único de cada producto
+    private String nombre;//nombre del producto
+    private double precio;//precio del producto
 
     public Producto(String nombre, double precio) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.idProducto = ++contador;
-        CantProducto++;
+        this.nombre = nombre; //guarda nombre
+        this.precio = precio; //guarda precio
+        this.idProducto = ++contador;//genera id automático
+        CantProducto++;//aumenta la cantidad total de productos
     }
     public String getNombre() {
         return nombre;
@@ -26,6 +26,8 @@ public class Producto {
     public int getIdProducto() {
         return idProducto;
     }
+
+    //Devuelve el producto en formato texto y Sirve para imprimirlo fácilmente
     @Override
     public String toString() {
         return "Producto [CantProducto=" + CantProducto + ", idProducto=" + idProducto + ", nombre=" + nombre
