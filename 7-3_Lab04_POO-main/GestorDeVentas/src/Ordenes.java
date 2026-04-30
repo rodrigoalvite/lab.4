140d1f503c0fc06ef7e306dc858b191149ff7024
rodrigoalvite <rodrigo.alvite07@gmail.com>
2026-04-30 13:55:44 -0300

    Update Ordenes.java

--- 7-3_Lab04_POO-main/GestorDeVentas/src/Ordenes.java
+++ 7-3_Lab04_POO-main/GestorDeVentas/src/Ordenes.java
@@ -6,12 +6,13 @@ public class Ordenes {
     private Producto[] productos;
     private double totalOrden = 0;
 
+    //Crea el array para guardar productos. maximo 10
     public Ordenes() {
         this.idOrden++;
         this.productos = new Producto[maxProductos];
     }
 
-    
+    //No controla si se supera el máximo de productos, podría dar error
     public void agregarProducto(Producto producto) {
         productos[cantProducto] = producto;
         cantProducto++;
@@ -27,15 +28,18 @@ public class Ordenes {
         return totalOrden;
     }
 
+    //muestra el numero de orden
     public void mostrarOrden(){
         System.out.println("Orden: " + idOrden);
         
-  
+
+        //recorre todo y te muestra el id nombre y precio
         for (int i = 0; i < cantProducto; i++) {
             System.out.println("ID: " + productos[i].getIdProducto() + " | Producto: " + 
             productos[i].getNombre() + " | Precio: " + productos[i].getPrecio());
             
         }
+        //Llama al método anterior y muestra el total
         System.out.println("Calculo Total: " + calcularTotal());
     }
 }
